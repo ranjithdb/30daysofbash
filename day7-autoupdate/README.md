a52424ff65be543daf5973334470fe21a041805f
ranjithdb <ranjithdbas@gmail.com>
2024-10-13 23:33:58 +0530

    fix cronjob

--- day7-autoupdate/README.md
+++ day7-autoupdate/README.md
@@ -1,3 +1,9 @@
+## Method 1(Unsafe)
+### Replace ```yourpassword``` with your actual password
+
+---
+
+## Method 2(DIDN'T WORK)
 ### configure sudo to allow your user to run the apt command without a password
 ### open the sudoers file with visudo command
 ```sudo visudo```
@@ -6,7 +12,9 @@
 ### If you want to allow all apt commands without a password, you can do this:
 ```yourusername ALL=(ALL) NOPASSWD: /usr/bin/apt```
 
+---
 
+## Cronjob to automate the update
 ### setup cronjob to run the script daily at 2am
 
 ```crontab -e```
@@ -14,6 +22,10 @@
 ### Add the following line at the end of the crontab file:
 
 ```0 2 * * * /path/to/auto-update.sh```
+
+### To list all cronjobs
+
+```crontab -l```
 
 ### To remove the all cronjobs
 
--- day7-autoupdate/auto-update.sh
+++ day7-autoupdate/auto-update.sh
@@ -2,25 +2,27 @@
 
 
 DATE=$(date +%d-%m-%Y)
-LOGFILE=updatelog_$DATE.log
+#LOGFILE=updatelog_$DATE.log
 
 #use absolute path for cronjob to work properly
-#LOGFILE=/home/ran/Desktop/updatelog_$DATE.log
+LOGFILE=/home/ran/Desktop/auto-updatelog_$DATE.log
+
+PASSWORD="yourpassword"
 
 echo "updating package list..." | tee -a "$LOGFILE"
-sudo apt update >> "$LOGFILE" 2>&1
+echo "$PASSWORD" | sudo -S apt update >> "$LOGFILE" 2>&1
 
 UPGRADE_LIST=$(apt list --upgradable)
 echo "Updates found for these packages...$UPGRADE_LIST" | tee -a "$LOGFILE"
 
 echo "Upgrading installed packages..." | tee -a "$LOGFILE"
-sudo apt upgrade -y >> "$LOGFILE" 2>&1
+echo "$PASSWORD" | sudo -S apt upgrade -y >> "$LOGFILE" 2>&1
 
 echo "Removing unnecessary packages..." | tee -a "$LOGFILE"
-sudo apt autoremove  -y >> "$LOGFILE" 2>&1
+echo "$PASSWORD" | sudo -S apt autoremove  -y >> "$LOGFILE" 2>&1
 
 echo "Cleaning up package cache..." | tee -a "$LOGFILE"
-sudo apt clean >> "$LOGFILE" 2>&1
+echo "$PASSWORD" | sudo -S apt clean >> "$LOGFILE" 2>&1
 
 
 echo "System updates and cleanup completed." | tee -a "$LOGFILE"
